d24673dd594d2ae3d7e398b4e467cc76ee9b28c4
Saiid Zafari <Saiid.Zafari@bahnhof.se>
2022-06-22 11:13:34 +0200

    InitialCommit

---
+++ src/webparts/bilhandel/components/IBilhandelProps.ts
@@ -0,0 +1,7 @@
+export interface IBilhandelProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/bilhandel/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IBilhandelWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'BilhandelWebPartStrings' {
+  const strings: IBilhandelWebPartStrings;
+  export = strings;
+}
